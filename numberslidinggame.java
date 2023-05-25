b5d0d550c7677c581e2abc8f7f7e634705a90818
Madisonhir <hirschbeckm@limestone.on.ca>
2023-05-25 09:26:41 -0400

    Update numberslidinggame.java

--- numberslidinggame.java
+++ numberslidinggame.java
@@ -1,8 +1,35 @@
-public class numberslidinggame{
-    public static void main(String[] args){
-        createTheGame(theGame);
-    }
-    public static int[][] createTheGame(int[][] theGame){
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class numberslidinggame {
+    public static void main(String[] args) {
+        int count = 0;
+        int[][] board = new int[4][4];
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            numbers.add(i);
+
+        }
+
+        Collections.shuffle(numbers);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = numbers.get(count);
+                count++;
+
+            }
+        }
+        printBoard(board);
 
     }
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.printf(" %2s ", board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
