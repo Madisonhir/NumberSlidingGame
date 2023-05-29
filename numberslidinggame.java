42ef4b4a75f2002b2be15adbb2b8a9a526e9f30c
Madisonhir <hirschbeckm@limestone.on.ca>
2023-05-29 08:06:35 -0400

    Update numberslidinggame.java

--- numberslidinggame.java
+++ numberslidinggame.java
@@ -29,31 +29,37 @@ public class numberslidinggame {
         int column1 = sc.nextInt();
         int rows2 = sc.nextInt();
         int column2 = sc.nextInt();
+        boolean isSolved = false;
         // swap
-        if (rows1 == rows2 + 1 || rows1 == rows2 - 1) {
-            if (column1 == column2 + 1 || column1 == column2 - 1) {
+        while (isSolved == false) {
+            if (board[0][0] == 1 && board[0][1] == 2 && board[0][2] == 3 && board[0][3] == 4 && board[1][0] == 5
+                    && board[1][1] == 6 && board[1][2] == 7 && board[1][3] == 8 && board[2][0] == 9 && board[2][1] == 10
+                    && board[2][2] == 11 && board[2][3] == 12 && board[3][0] == 13 && board[3][1] == 14
+                    && board[3][2] == 15 && board[3][3] == 0) {
+                isSolved = true;
+            }
+            if ((rows1 == rows2 + 1 || rows1 == rows2 - 1 || rows1 == rows2)
+                    && (column1 == column2 + 1 || column1 == column2 - 1 || column1 == column2)
+                    && (board[rows1][column1] == 0 || board[rows2][column2] == 0)) {
                 int temp = board[rows1][column1];
                 board[rows1][column1] = board[rows2][column2];
                 board[rows2][column2] = temp;
-            }else{
+                printBoard(board);
+            } else {
                 System.out.println("Invalid. Try Again.");
                 rows1 = sc.nextInt();
                 column1 = sc.nextInt();
                 rows2 = sc.nextInt();
                 column2 = sc.nextInt();
-                printBoard(board);
+
             }
-        }else{
-            System.out.println("Invalid. Try Again.");
+
             rows1 = sc.nextInt();
             column1 = sc.nextInt();
             rows2 = sc.nextInt();
             column2 = sc.nextInt();
-            printBoard(board);
-        }
-       
 
-       
+        }
 
     }
 
